1a036fcedf94580dd293df2ce0aa070526782aa7
PaulinaArvizu <is707571@iteso.mx>
2020-10-14 17:21:57 -0500

    Agregar TextFormFields

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(LogIn());
+import 'package:wag_proyecto_moviles/colors.dart';
 
 class LogIn extends StatefulWidget {
   @override
@@ -13,29 +12,103 @@ class _LogInState extends State<LogIn> {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset(
-                    "assets/images/Logo wag.png",
-                    height: 100,
-                  ),
-                  Container(
-                    child: Text(
+        body: Container(
+          color: background,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/images/Logo wag.png",
+                      height: 100,
+                    ),
+                    Text(
                       "WAG",
                       style: TextStyle(
                         fontFamily: 'Roboto Black Italic',
                         fontSize: 46,
                       ),
                     ),
+                  ],
+                ),
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    fontFamily: 'Poppins Bold',
+                    fontSize: 30,
                   ),
-                ],
-              ),
-            ],
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding:
+                      EdgeInsets.only(left: 30, right: 30, top: 20, bottom: 15),
+                  child: TextFormField(
+                    controller: null,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xffE2E2E2), width: 0.6),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: primary, width: 0.6),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      contentPadding: EdgeInsets.only(
+                        left: 30,
+                        top: 20,
+                        bottom: 20,
+                      ),
+                      hintText: "Email Address",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontFamily: 'Poppins Regular',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.only(left: 30, right: 30, bottom: 10),
+                  child: TextFormField(
+                    controller: null,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xffE2E2E2), width: 0.6),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: primary, width: 0.6),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      contentPadding: EdgeInsets.only(
+                        left: 30,
+                        top: 20,
+                        bottom: 20,
+                      ),
+                      hintText: "Password",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontFamily: 'Poppins Regular',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  "FORGOT PASSWORD?",
+                  style: TextStyle(
+                    fontFamily: 'Poppins Medium',
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
