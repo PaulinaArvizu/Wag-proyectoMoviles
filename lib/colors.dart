f0487180322b82a8ec8fc82b1c8ac1898f8e296b
PaulinaArvizu <is707571@iteso.mx>
2020-10-13 22:16:23 -0500

    Definir colores

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const background = Color(0xffF6FAFF);
+const primary = Color(0xffe3097d);
+const secondary = Color(0xff2208DC);
+const secondary_background = Color(0xffEEECFC);
+const subtotal_background = Color(0xffE1EDFC);
+const pagar_background = Color(0xff3FD187);
