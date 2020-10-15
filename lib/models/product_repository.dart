ba1c610a0e84e09ad1adedbf045b4b1bf64453fa
PaulinaArvizu <is707571@iteso.mx>
2020-10-15 00:47:16 -0500

    Agregar algunos productos

--- lib/models/product_repository.dart
+++ lib/models/product_repository.dart
@@ -10,7 +10,30 @@ class ProductRepository {
         productAmount: 0,
         productImage:
             'https://www.petco.com.mx/medias/?context=bWFzdGVyfGltYWdlc3wzNDUzOTZ8aW1hZ2UvanBlZ3xpbWFnZXMvaDA2L2hmZC85MjI3NzQ1MDAxNTAyLmpwZ3w5ZGQyYjUwZGU3YzUxODIzNjFlZTQzNjkyZDk1Yjc4MGU1MGIxYTk1MDZiMjhiMGJjM2EzZWZhN2FmYmFhMTc1',
-        productSize: ProductSize.M,
+        productPrice: 100,
+      ),
+      ProductItem(
+        productTitle: 'Taining Treats',
+        productDescription: 'Premios para perros de cualquier edad.',
+        productAmount: 0,
+        productImage:
+            'https://images-na.ssl-images-amazon.com/images/I/81zOhh0vRnL._AC_SX522_.jpg',
+        productPrice: 100,
+      ),
+      ProductItem(
+        productTitle: 'Taining Treats',
+        productDescription: 'Premios para perros de cualquier edad.',
+        productAmount: 0,
+        productImage:
+            'https://http2.mlstatic.com/pet-products-encanto-parches-grande-del-juguete-del-gallo-D_NQ_NP_662116-MLM27025407711_032018-F.jpg',
+        productPrice: 100,
+      ),
+      ProductItem(
+        productTitle: 'Taining Treats',
+        productDescription: 'Premios para perros de cualquier edad.',
+        productAmount: 0,
+        productImage:
+            'https://www.costco.com.mx/medias/sys_master/products/h68/h4c/10764142084126.jpg',
         productPrice: 100,
       ),
     ];
