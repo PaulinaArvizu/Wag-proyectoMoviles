0591114f99cd9069ae2a9c6e8e49a5023bfa5edc
PaulinaArvizu <is707571@iteso.mx>
2020-10-15 00:47:45 -0500

    Eliminado ProductSize

--- lib/models/product_item.dart
+++ lib/models/product_item.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/foundation.dart';
 
-enum ProductSize { CH, M, G }
-
 class ProductItem {
   final String productTitle; // nombre del producto
   final String productDescription; // descripcion del producto
   final String productImage; // url de imagen del producto
-  ProductSize productSize; // tamano del producto
   double productPrice; // precio del producto autocalculado
   int productAmount; // cantidad de producto por comprar
 
@@ -14,7 +11,6 @@ class ProductItem {
     @required this.productTitle,
     @required this.productDescription,
     @required this.productImage,
-    @required this.productSize,
     @required this.productAmount,
     @required this.productPrice,
   });
