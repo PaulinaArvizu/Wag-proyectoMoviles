324b0ab915e3a706a18db6f2a996412c9d47658d
PaulinaArvizu <is707571@iteso.mx>
2020-10-14 20:10:14 -0500

    Ajustes de vista. Agregar Boton de iniciar con Google

--- lib/inicio/login.dart
+++ lib/inicio/login.dart
@@ -7,153 +7,216 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        body: Container(
-          color: background,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset(
-                      "assets/images/Logo wag.png",
-                      height: 100,
+        backgroundColor: background,
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.symmetric(horizontal: 22),
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    "assets/images/Logo wag.png",
+                    height: 100,
+                  ),
+                  Text(
+                    "WAG",
+                    style: TextStyle(
+                      fontFamily: 'Roboto Black Italic',
+                      fontSize: 46,
                     ),
-                    Text(
-                      "WAG",
-                      style: TextStyle(
-                        fontFamily: 'Roboto Black Italic',
-                        fontSize: 46,
-                      ),
-                    ),
-                  ],
+                  ),
+                ],
+              ),
+              Text(
+                "Login",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Poppins Bold',
+                  fontSize: 30,
                 ),
-                Text(
-                  "Login",
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: TextFormField(
+                  controller: null,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xffE2E2E2), width: 0.6),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: primary, width: 0.6),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    contentPadding: EdgeInsets.only(
+                      left: 30,
+                      top: 20,
+                      bottom: 20,
+                    ),
+                    hintText: "Email Address",
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Poppins Regular',
+                      fontSize: 18,
+                    ),
+                  ),
                   style: TextStyle(
-                    fontFamily: 'Poppins Bold',
-                    fontSize: 30,
+                    fontFamily: 'Poppins Regular',
+                    fontSize: 18,
                   ),
                 ),
-                Container(
-                  alignment: Alignment.center,
-                  padding:
-                      EdgeInsets.only(left: 22, right: 22, top: 20, bottom: 15),
-                  child: TextFormField(
-                    controller: null,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Color(0xffE2E2E2), width: 0.6),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: primary, width: 0.6),
-                      ),
-                      fillColor: Colors.white,
-                      filled: true,
-                      contentPadding: EdgeInsets.only(
-                        left: 30,
-                        top: 20,
-                        bottom: 20,
-                      ),
-                      hintText: "Email Address",
-                      hintStyle: TextStyle(
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(bottom: 20),
+                child: TextFormField(
+                  controller: null,
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xffE2E2E2), width: 0.6),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: primary, width: 0.6),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    contentPadding: EdgeInsets.only(
+                      left: 30,
+                      top: 20,
+                      bottom: 20,
+                    ),
+                    hintText: "Password",
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Poppins Regular',
+                      fontSize: 18,
+                    ),
+                    suffixIcon: Padding(
+                      padding: EdgeInsets.only(top: 1.0),
+                      child: IconButton(
                         color: Colors.grey,
-                        fontFamily: 'Poppins Regular',
-                        fontSize: 18,
+                        icon: Icon(
+                          _obscureText
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(
+                            () {
+                              _obscureText = !_obscureText;
+                            },
+                          );
+                        },
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.only(left: 22, right: 22, bottom: 20),
-                  child: TextFormField(
-                    controller: null,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Color(0xffE2E2E2), width: 0.6),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: primary, width: 0.6),
-                      ),
-                      fillColor: Colors.white,
-                      filled: true,
-                      contentPadding: EdgeInsets.only(
-                        left: 30,
-                        top: 20,
-                        bottom: 20,
-                      ),
-                      hintText: "Password",
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontFamily: 'Poppins Regular',
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ),
-                Text(
-                  "FORGOT PASSWORD?",
                   style: TextStyle(
-                    fontFamily: 'Poppins SemiBold',
-                    fontSize: 12,
-                    color: Color(0xFF707070),
+                    fontFamily: 'Poppins Regular',
+                    fontSize: 18,
                   ),
                 ),
-                Container(
-                  alignment: Alignment.center,
-                  padding:
-                      EdgeInsets.only(left: 22, right: 22, bottom: 20, top: 20),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: FlatButton(
-                      padding: EdgeInsets.all(20),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          side: BorderSide(color: primary)),
-                      onPressed: () {},
-                      color: primary,
-                      textColor: Colors.white,
-                      child: Text("LOGIN",
-                          style: TextStyle(
-                            fontFamily: 'Poppins SemiBold',
-                            fontSize: 16,
-                          )),
-                    ),
-                  ),
+              ),
+              Text(
+                "FORGOT PASSWORD?",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Poppins SemiBold',
+                  fontSize: 12,
+                  color: Color(0xFF707070),
                 ),
-                Container(
-                  alignment: Alignment.center,
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: SizedBox(
+                  width: double.infinity,
                   child: FlatButton(
-                    // padding: EdgeInsets.all(20),
+                    padding: EdgeInsets.all(20),
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      side: BorderSide(color: secondary_background),
-                    ),
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(color: primary)),
                     onPressed: () {},
-                    color: secondary_background,
-                    textColor: secondary,
-                    child: Text(
-                      "SIGN UP",
-                      style: TextStyle(
-                        fontFamily: 'Poppins SemiBold',
-                        fontSize: 12,
-                      ),
+                    color: primary,
+                    textColor: Colors.white,
+                    child: Text("LOGIN",
+                        style: TextStyle(
+                          fontFamily: 'Poppins SemiBold',
+                          fontSize: 16,
+                        )),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    side: BorderSide(color: secondary_background),
+                  ),
+                  onPressed: () {},
+                  color: secondary_background,
+                  textColor: secondary,
+                  child: Text(
+                    "SIGN UP",
+                    style: TextStyle(
+                      fontFamily: 'Poppins SemiBold',
+                      fontSize: 12,
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              Text(
+                "OR",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Poppins SemiBold',
+                  fontSize: 12,
+                  color: Color(0xFF707070),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    side: BorderSide(color: Color(0xffE2E2E2)),
+                  ),
+                  onPressed: () {},
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.network(
+                        "https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png",
+                        height: 16,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "SIGN IN WITH GOOGLE",
+                        style: TextStyle(
+                          fontFamily: 'Poppins SemiBold',
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
