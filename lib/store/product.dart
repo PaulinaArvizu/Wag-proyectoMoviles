0830cdc4c57fd7ce5161f67711553e16cbad3230
PaulinaArvizu <is707571@iteso.mx>
2020-10-15 00:46:56 -0500

    Tarjetas de productos

---
+++ lib/store/product.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:wag_proyecto_moviles/colors.dart';
+import 'package:wag_proyecto_moviles/models/product_item.dart';
+import 'package:wag_proyecto_moviles/models/product_repository.dart';
+
+class Product extends StatefulWidget {
+  final ProductItem product;
+  Product({
+    Key key,
+    @required this.product,
+  }) : super(key: key);
+
+  @override
+  _ProductState createState() => _ProductState();
+}
+
+class _ProductState extends State<Product> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: background,
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8 / 10,
+          children: _buildGridCards(context) // Changed code
+          ),
+    );
+  }
+}
+
+List<Card> _buildGridCards(BuildContext context) {
+  List<ProductItem> products = ProductRepository.loadProducts();
+
+  if (products == null || products.isEmpty) {
+    return const <Card>[];
+  }
+
+  return products.map((product) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Container(
+        padding: EdgeInsets.only(top: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(
+              flex: 9,
+              child: AspectRatio(
+                aspectRatio: 18 / 11,
+                child: SizedBox(
+                  child: Image.network(
+                    product.productImage,
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 10,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Text(
+                      product.productTitle,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      "\$${product.productPrice.toStringAsFixed(2)} MXN",
+                      style: TextStyle(
+                        fontFamily: 'Poppins Bold',
+                        fontSize: 18,
+                        color: primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }).toList();
+}
