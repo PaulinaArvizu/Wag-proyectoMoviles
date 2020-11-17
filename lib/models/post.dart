98d0a0b8662340a2da3e56f5d6f60ed5f9b3d0c6
PaulinaArvizu <is707571@iteso.mx>
2020-11-17 14:46:54 -0600

    Definir clase Post

---
+++ lib/models/post.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+
+class Post {
+  final String name;
+  final String size;
+  final String imageUrl;
+  final String age;
+  final String description;
+  final String authorID;
+  final String date;
+
+  Post({
+    @required this.name,
+    @required this.size,
+    @required this.imageUrl,
+    @required this.age,
+    @required this.description,
+    @required this.authorID,
+    @required this.date,
+  });
+}
