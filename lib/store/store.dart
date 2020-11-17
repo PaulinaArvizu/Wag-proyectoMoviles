c8f9af2a4a65234c5f2e3900dbb5c18731990db2
PaulinaArvizu <is707571@iteso.mx>
2020-11-17 14:56:08 -0600

    Quitar navigations que BottomNavigation reemplazó

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:wag_proyecto_moviles/cart/cart.dart';
 import 'package:wag_proyecto_moviles/colors.dart';
-import 'package:wag_proyecto_moviles/home/home_page.dart';
 import 'package:wag_proyecto_moviles/models/product_item_cart.dart';
 import 'package:wag_proyecto_moviles/models/product_repository.dart';
-import 'package:wag_proyecto_moviles/profile/profile.dart';
 import 'package:wag_proyecto_moviles/store/product.dart';
 
 class Store extends StatefulWidget {
@@ -18,7 +16,6 @@ class Store extends StatefulWidget {
 }
 
 class _StoreState extends State<Store> {
-  int _selectedIndex = 2;
   List<ProductItemCart> productList = new List<ProductItemCart>();
   @override
   Widget build(BuildContext context) {
@@ -55,45 +52,19 @@ class _StoreState extends State<Store> {
     );
   }
 
-  void _openHomePage() {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) {
-        return HomePage();
-      }),
-    );
-  }
-
   void _openCartPage() {
     Navigator.of(context).push(
       MaterialPageRoute(builder: (context) {
         return Cart(
-          productsList: ProductRepository.loadProducts().map(
-            (product) {
-              return new ProductItemCart(
-                productTitle: product.productTitle,
-                productImage: product.productImage,
-                productAmount: 0,
-                productPrice: product.productPrice,
-              );
-            },
-          ).toList(),
+          productsList: ProductRepository.loadProducts().map((product) {
+            return new ProductItemCart(
+              productTitle: product.productTitle,
+              productImage: product.productImage,
+              productAmount: 0,
+              productPrice: product.productPrice,
+            );
+          }).toList(),
         );
-      }),
-    );
-  }
-
-  void _openStorePage() {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) {
-        return Store();
-      }),
-    );
-  }
-
-  void _openProfilePage() {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) {
-        return Profile();
       }),
     );
   }
