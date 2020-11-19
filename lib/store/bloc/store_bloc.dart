84a7cd4b79b66b2cb277ace27a43cefe35937ff9
PaulinaArvizu <is707571@iteso.mx>
2020-11-18 20:13:05 -0600

    Crear bloc para store

---
+++ lib/store/bloc/store_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'store_event.dart';
+part 'store_state.dart';
+
+class StoreBloc extends Bloc<StoreEvent, StoreState> {
+  StoreBloc() : super(StoreInitial());
+
+  @override
+  Stream<StoreState> mapEventToState(
+    StoreEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
