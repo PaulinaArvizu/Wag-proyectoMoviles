a193259705c65ff2a59eecd00f4c7c4e26e36b7a
PaulinaArvizu <is707571@iteso.mx>
2020-11-18 23:02:55 -0600

    Implementacion de bloc para agregar productos a carrito

--- lib/store/product_detail.dart
+++ lib/store/product_detail.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wag_proyecto_moviles/colors.dart';
 import 'package:wag_proyecto_moviles/models/product_item.dart';
+import 'package:wag_proyecto_moviles/models/product_item_cart.dart';
+
+import 'bloc/store_bloc.dart';
 
 class ProductDetail extends StatefulWidget {
   final ProductItem product;
@@ -15,117 +19,140 @@ class ProductDetail extends StatefulWidget {
 
 class _ProductDetailState extends State<ProductDetail> {
   bool expandedText = false;
+  StoreBloc _storeBloc;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: background,
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(20),
-              padding: EdgeInsets.all(10),
-              child: Image.network(widget.product.productImage),
-              constraints: BoxConstraints(
-                maxHeight: 300,
-                maxWidth: 200,
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 2,
-                  color: Color(0xffE2E2E2),
-                ),
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                widget.product.productTitle,
-                style: TextStyle(
-                  fontFamily: 'Poppins SemiBold',
-                  fontSize: 22,
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                "\$${widget.product.productPrice.toStringAsFixed(2)} MXN",
-                style: TextStyle(
-                  fontFamily: 'Poppins Bold',
-                  fontSize: 28,
-                  color: primary,
-                ),
-              ),
-            ),
-            SizedBox(height: 25),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: Container(
-                    margin: EdgeInsets.only(left: 30),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: expandedText
-                          ? Text(
-                              widget.product.productDescription,
-                              style: TextStyle(fontSize: 18),
-                            )
-                          : Text(
-                              widget.product.productDescription,
-                              style: TextStyle(fontSize: 18),
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
+      body: BlocProvider(
+        create: (context) {
+          _storeBloc = StoreBloc();
+          return _storeBloc;
+        },
+        child: BlocBuilder<StoreBloc, StoreState>(
+          builder: (context, state) {
+            if (state is StoreInitial) {}
+            return Center(
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    padding: EdgeInsets.all(10),
+                    child: Image.network(widget.product.productImage),
+                    constraints: BoxConstraints(
+                      maxHeight: 300,
+                      maxWidth: 200,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 2,
+                        color: Color(0xffE2E2E2),
+                      ),
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    child: Text(
+                      widget.product.productTitle,
+                      style: TextStyle(
+                        fontFamily: 'Poppins SemiBold',
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    child: Text(
+                      "\$${widget.product.productPrice.toStringAsFixed(2)} MXN",
+                      style: TextStyle(
+                        fontFamily: 'Poppins Bold',
+                        fontSize: 28,
+                        color: primary,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: Container(
+                          margin: EdgeInsets.only(left: 30),
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: expandedText
+                                ? Text(
+                                    widget.product.productDescription,
+                                    style: TextStyle(fontSize: 18),
+                                  )
+                                : Text(
+                                    widget.product.productDescription,
+                                    style: TextStyle(fontSize: 18),
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: IconButton(
+                          icon: expandedText
+                              ? Icon(Icons.arrow_circle_up)
+                              : Icon(Icons.arrow_circle_down),
+                          onPressed: () {
+                            setState(() {
+                              expandedText = !expandedText;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 20,
+                    ),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: FlatButton(
+                        padding: EdgeInsets.all(15),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(color: primary)),
+                        onPressed: () {
+                          _storeBloc.add(
+                            AddToCartEvent(
+                              product: ProductItemCart(
+                                productAmount: 1,
+                                productImage: widget.product.productImage,
+                                productPrice: widget.product.productPrice,
+                                productTitle: widget.product.productTitle,
+                              ),
                             ),
+                          );
+                        },
+                        color: primary,
+                        textColor: Colors.white,
+                        child: Text(
+                          "ADD TO CART",
+                          style: TextStyle(
+                            fontFamily: 'Poppins Regular',
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: IconButton(
-                    icon: expandedText
-                        ? Icon(Icons.arrow_circle_up)
-                        : Icon(Icons.arrow_circle_down),
-                    onPressed: () {
-                      setState(() {
-                        expandedText = !expandedText;
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.symmetric(
-                horizontal: 30,
-                vertical: 20,
+                ],
               ),
-              child: SizedBox(
-                width: double.infinity,
-                child: FlatButton(
-                  padding: EdgeInsets.all(15),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(color: primary)),
-                  onPressed: () {},
-                  color: primary,
-                  textColor: Colors.white,
-                  child: Text(
-                    "ADD TO CART",
-                    style: TextStyle(
-                      fontFamily: 'Poppins Regular',
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
