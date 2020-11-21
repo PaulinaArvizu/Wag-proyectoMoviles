8cb4a6c0760eada05e4187e1a4e639a911cdffca
Héctor Gerardo Silva Ayala <is712655@iteso.mx>
2020-11-20 21:50:42 -0600

    Update cart.dart

--- lib/store/cart/cart.dart
+++ lib/store/cart/cart.dart
@@ -60,7 +60,17 @@ class _CartState extends State<Cart> {
               _productsList = state.productsList;
               if (_productsList.length == 0) {
                 return Center(
-                  child: Text("Your cart is empty"),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/EmptyCart.png',
+                        height: 75,
+                      ),
+                      SizedBox(height: 10),
+                      Text("Your cart is empty"),
+                    ],
+                  ),
                 );
               }
               _total = 0;
@@ -69,135 +79,106 @@ class _CartState extends State<Cart> {
               }
             }
 
-            return Column(
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    itemCount: _productsList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ItemCart(
-                        // onAmountUpdated: _priceUpdate,
-                        item: _productsList[index],
-                        itemIndex: index,
-                        storeBloc: _storeBloc,
-                      );
-                    },
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Card(
-                      color: subtotal_background,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      margin: EdgeInsets.symmetric(vertical: 15),
-                      child: Column(
-                        children: [
-                          SizedBox(height: 5),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                "Subtotal: ",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontFamily: 'Proxima Nova Regular',
-                                ),
-                              ),
-                              Text(
-                                "\$${_total.toStringAsFixed(2)} MXN",
-                                style: TextStyle(
-                                    fontSize: 30,
-                                    fontFamily: 'Proxima Nova Regular'),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 20),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              alignment: Alignment.center,
-                              padding: EdgeInsets.symmetric(horizontal: 45),
-                              child: SizedBox(
-                                width: double.infinity,
-                                child: FlatButton(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  onPressed: () {},
-                                  color: pagar_background,
-                                  textColor: Colors.white,
-                                  child: Text("PAGAR",
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins SemiBold',
-                                        fontSize: 20,
-                                      )),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+            return _productsList.length == 0
+                ? Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/EmptyCart.png',
+                          height: 75,
+                        ),
+                        SizedBox(height: 10),
+                        Text("Your cart is empty"),
+                      ],
                     ),
-                  ),
-                ),
-              ],
-            );
+                  )
+                : _cart();
           },
         ),
       ),
     );
-    // Scaffold(
-    //   appBar: AppBar(
-    //     title: Text('Carrito de Compras'),
-    //   ),
-    //   body: Stack(
-    //     children: <Widget>[
-    //       Container(
-    //         height: MediaQuery.of(context).size.height * 0.9,
-    //         child: ListView.builder(
-    //           itemCount: widget.productsList.length,
-    //           itemBuilder: (BuildContext context, int index) {
-    //             return ItemCart(
-    //               onAmountUpdated: _priceUpdate,
-    //               cartItemsList:
-    //                   widget.productsList, //widget.productsList[index],
-    //             );
-    //           },
-    //         ),
-    //       ),
-    //       Positioned(
-    //         bottom: 50,
-    //         child: Text("Total: \$$_total"),
-    //       ),
-    //       Positioned(
-    //         bottom: 0,
-    //         child: RaisedButton(
-    //           onPressed: () {
-    //             Navigator.of(context).pop();
-    //             Navigator.of(context)
-    //                 .push(MaterialPageRoute(builder: (context) {
-    //               return Payment();
-    //             }));
-    //           },
-    //           textColor: Colors.white,
-    //           padding: const EdgeInsets.all(0.0),
-    //           child: Container(
-    //             padding: const EdgeInsets.all(10.0),
-    //             child: const Text('Pagar', style: TextStyle(fontSize: 20)),
-    //           ),
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // );
+  }
+
+  Widget _cart() {
+    return Column(
+      children: [
+        Expanded(
+          flex: 3,
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            itemCount: _productsList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ItemCart(
+                item: _productsList[index],
+                itemIndex: index,
+                storeBloc: _storeBloc,
+              );
+            },
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Card(
+              color: subtotal_background,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              margin: EdgeInsets.symmetric(vertical: 15),
+              child: Column(
+                children: [
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Subtotal: ",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Proxima Nova Regular',
+                        ),
+                      ),
+                      Text(
+                        "\$${_total.toStringAsFixed(2)} MXN",
+                        style: TextStyle(
+                            fontSize: 30, fontFamily: 'Proxima Nova Regular'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(horizontal: 45),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        onPressed: () {},
+                        color: pagar_background,
+                        textColor: Colors.white,
+                        child: Text(
+                          "PAGAR",
+                          style: TextStyle(
+                            fontFamily: 'Poppins SemiBold',
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   void _updateTotal() {
@@ -208,11 +189,4 @@ class _CartState extends State<Cart> {
       }
     });
   }
-
-  // void _priceUpdate(double newItemPrice) {
-  //   setState(() {
-  //     _total += newItemPrice;
-  //     if (_total < 0) _total = 0;
-  //   });
-  // }
 }
