6d7e79202a4c9d7c39a718e59a5ca362d0a02350
PaulinaArvizu <is707571@iteso.mx>
2020-11-20 21:51:18 -0600

    Implementacion de busqueda de posts

--- lib/home/feed.dart
+++ lib/home/feed.dart
@@ -10,54 +10,84 @@ class Feed extends StatefulWidget {
 }
 
 class _FeedState extends State<Feed> {
-  Post _post = Post(
-    name: 'Perritu',
-    size: 'Grande',
-    imageUrl: '',
-    age: '3 años',
-    description:
-        'Mucho texto mucho texto mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto ',
-    authorID: null,
-    date: null,
-    contactInfo: 'Holis',
-  );
+  List<Post> _posts = [
+    Post(
+      name: 'Perritu',
+      size: 'Grande',
+      imageUrl: '',
+      age: '3 años',
+      description:
+          'Mucho texto mucho texto mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto ',
+      authorID: null,
+      date: null,
+      contactInfo: 'Holis',
+    ),
+    Post(
+      name: 'Hola',
+      size: 'adios',
+      imageUrl: '',
+      age: '1 años',
+      description:
+          'Mucho texto mucho texto mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto  mucho texto ',
+      authorID: null,
+      date: null,
+      contactInfo: 'jelou',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                "assets/images/Logo wag.png",
-                height: 100,
+    return Scaffold(
+      backgroundColor: background,
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 90,
+        backgroundColor: background,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              "assets/images/Logo wag.png",
+              height: 85,
+            ),
+            SizedBox(width: 5),
+            Text(
+              "Wag",
+              style: TextStyle(
+                fontFamily: 'Roboto Black Italic',
+                fontSize: 46,
+                color: Colors.black,
               ),
-              SizedBox(width: 5),
-              Text(
-                "WAG",
-                style: TextStyle(
-                  fontFamily: 'Roboto Black Italic',
-                  fontSize: 46,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-        Expanded(
-          flex: 6,
-          // aqui empiezan los posts
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            // padding: EdgeInsets.symmetric(horizontal: 20),
-            itemCount: 2,
-            itemBuilder: (BuildContext context, int index) {
-              return _postFormat(_post);
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: Search(
+                  posts: _posts,
+                  postFormat: _postFormat,
+                ),
+              );
             },
           ),
-        ),
-      ],
+        ],
+      ),
+      body: ListView.builder(
+        scrollDirection: Axis.vertical,
+        // padding: EdgeInsets.symmetric(horizontal: 20),
+        itemCount: _posts.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _postFormat(_posts[index]);
+        },
+      ),
     );
   }
 
@@ -245,5 +275,75 @@ class _FeedState extends State<Feed> {
         );
       },
     );
+  }
+}
+
+class Search extends SearchDelegate {
+  final List<Post> posts;
+  final Function postFormat;
+
+  Search({
+    @required this.posts,
+    @required this.postFormat,
+  });
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    //boton que limpie la busqueda
+    return <Widget>[
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    //leading icon (a la izquierda del search)
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<Post> filteredPosts = [];
+    //busca los resultados
+    query.isEmpty
+        ? filteredPosts = []
+        : filteredPosts = posts.where((p) {
+            String name = p.name.toLowerCase();
+            String size = p.size.toLowerCase();
+            String age = p.age.toLowerCase();
+            String desc = p.description.toLowerCase();
+            String search = query.toLowerCase();
+            return name.contains(search) ||
+                size.contains(search) ||
+                age.contains(search) ||
+                desc.contains(search);
+          }).toList();
+
+    //muestra los resultados
+    return filteredPosts.length == 0
+        ? Center(child: Text('No results'))
+        : ListView.builder(
+            scrollDirection: Axis.vertical,
+            // padding: EdgeInsets.symmetric(horizontal: 20),
+            itemCount: filteredPosts.length,
+            itemBuilder: (BuildContext context, int index) {
+              return postFormat(filteredPosts[index]);
+            },
+          );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Center(child: Text(''));
   }
 }
