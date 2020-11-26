9b6c4e9097464c9f1234710cbd68c66c9d51f700
PaulinaArvizu <is707571@iteso.mx>
2020-11-25 21:54:32 -0600

    Agregar estados de success y error

--- lib/home/bloc/home_state.dart
+++ lib/home/bloc/home_state.dart
@@ -2,9 +2,19 @@ part of 'home_bloc.dart';
 
 abstract class HomeState extends Equatable {
   const HomeState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class HomeInitial extends HomeState {}
+
+class HomeSuccessState extends HomeState {}
+
+class HomeErrorState extends HomeState {
+  final String errorMessage;
+
+  HomeErrorState({@required this.errorMessage});
+  @override
+  List<Object> get props => [errorMessage];
+}
