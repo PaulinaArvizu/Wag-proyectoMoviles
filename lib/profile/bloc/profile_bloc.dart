89587e747f4208497a4e1d613567fd479a7890fb
PaulinaArvizu <is707571@iteso.mx>
2020-11-27 20:12:44 -0600

    Remove unused import

--- lib/profile/bloc/profile_bloc.dart
+++ lib/profile/bloc/profile_bloc.dart
@@ -7,7 +7,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as Path;
 import 'package:wag_proyecto_moviles/models/post.dart';
