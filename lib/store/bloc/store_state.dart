e78c98800ce53a09972301ce55b4dfa89d941bd3
PaulinaArvizu <is707571@iteso.mx>
2020-11-30 20:56:35 -0600

    Crear estados de error y success

--- lib/store/bloc/store_state.dart
+++ lib/store/bloc/store_state.dart
@@ -26,3 +26,19 @@ class CartUpdatedState extends StoreState {}
 class EmptyCartState extends StoreState {}
 
 class StandbyState extends StoreState {}
+
+class StoreErrorState extends StoreState {
+  final String errorMessage;
+
+  StoreErrorState({@required this.errorMessage});
+  @override
+  List<Object> get props => [errorMessage];
+}
+
+class StoreSuccessState extends StoreState {
+  final List<ProductItem> productsList;
+
+  StoreSuccessState({@required this.productsList});
+  @override
+  List<Object> get props => [productsList];
+}
